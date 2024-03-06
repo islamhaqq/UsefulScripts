46f54d2a04c78c94d83d5fc35fd4205b09e20d17
Syed Aman <syednashikaman@gmail.com>
2024-03-06 15:54:00 -0500

    Accept extensions

--- copy-files-to-clip.ps1
+++ copy-files-to-clip.ps1
@@ -1,17 +1,25 @@
+param(
+    [string[]]$Extensions = @("*.ts", "*.tsx", "*.js", "*.jsx")
+)
+
 # Create an empty array to store file contents
 $allFilesContent = @()
 
-# Get all the files with the specified extensions
-$files = Get-ChildItem -Path .\ -Include *.ts, *.tsx, *.js, *.jsx -Recurse
+# Get all the files with the specified extensions in the current directory and subdirectories
+foreach ($ext in $Extensions) {
+    $files = Get-ChildItem -Path .\ -Filter $ext -Recurse
+    # Loop through each file
+    foreach ($file in $files) {
+        # Calculate the relative path from the current directory
+        $relativePath = $file.FullName.Substring((Get-Location).Path.Length + 1)
 
-# Loop through each file
-foreach ($file in $files) {
-    # Add file name with comment format to the array
-    $allFilesContent += "// " + $file.Name
-    # Add file content to the array
-    $allFilesContent += Get-Content $file.FullName
-    # Add a new line for separation between files
-    $allFilesContent += "`n"
+        # Add file path with comment format to the array
+        $allFilesContent += "// " + $relativePath
+        # Add file content to the array
+        $allFilesContent += Get-Content $file.FullName
+        # Add a new line for separation between files
+        $allFilesContent += "`n"
+    }
 }
 
 # Convert the array to a single string
