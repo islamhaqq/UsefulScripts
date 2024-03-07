9fb21c45aed49a30ad61747e5eef59d165deb18b
Syed Aman <syednashikaman@gmail.com>
2024-03-07 11:35:27 -0500

    Do not try to merge directories.

--- MergeFilesAndCopy.ps1
+++ MergeFilesAndCopy.ps1
@@ -5,20 +5,23 @@ param(
 # Create an empty array to store file contents
 $allFilesContent = @()
 
-# Get all the files with the specified extensions in the current directory and subdirectories
+# Get all the items with the specified extensions in the current directory and subdirectories
 foreach ($ext in $Extensions) {
-    $files = Get-ChildItem -Path .\ -Filter $ext -Recurse
-    # Loop through each file
-    foreach ($file in $files) {
-        # Calculate the relative path from the current directory
-        $relativePath = $file.FullName.Substring((Get-Location).Path.Length + 1)
+    $items = Get-ChildItem -Path .\ -Filter $ext -Recurse
+    # Loop through each item
+    foreach ($item in $items) {
+        # Check if the item is a file
+        if ($item -is [System.IO.FileInfo]) {
+            # Calculate the relative path from the current directory
+            $relativePath = $item.FullName.Substring((Get-Location).Path.Length + 1)
 
-        # Add file path with comment format to the array
-        $allFilesContent += "// " + $relativePath
-        # Add file content to the array
-        $allFilesContent += Get-Content $file.FullName
-        # Add a new line for separation between files
-        $allFilesContent += "`n"
+            # Add file path with comment format to the array
+            $allFilesContent += "// " + $relativePath
+            # Add file content to the array
+            $allFilesContent += Get-Content $item.FullName
+            # Add a new line for separation between files
+            $allFilesContent += "`n"
+        }
     }
 }
 
