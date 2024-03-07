6a397158f6ece09045de0a713150a70533fc41b5
Syed Aman <syednashikaman@gmail.com>
2024-03-07 11:37:45 -0500

    Ignore directories or files

--- MergeFilesAndCopy.ps1
+++ MergeFilesAndCopy.ps1
@@ -1,13 +1,29 @@
 param(
-    [string[]]$Extensions = @("*.ts", "*.tsx", "*.js", "*.jsx")
+    [string[]]$Extensions = @("*"), # Default is all files if not specified
+    [string[]]$Ignore = @("node_modules", "*.json") # Default values to ignore if not specified
 )
 
 # Create an empty array to store file contents
 $allFilesContent = @()
 
+# Convert ignore patterns to array of regex for matching paths and filenames
+$ignorePatterns = $Ignore -replace '\.', '\\.' -replace '\*', '.*' -replace '\?', '.'
+$ignoreRegex = $ignorePatterns | ForEach-Object { [regex]$_ }
+
+# Define a helper function to check if the file or path should be ignored
+function Should-Ignore {
+    param([string]$Path)
+    foreach ($pattern in $global:ignoreRegex) {
+        if ($Path -match $pattern) {
+            return $true
+        }
+    }
+    return $false
+}
+
 # Get all the items with the specified extensions in the current directory and subdirectories
 foreach ($ext in $Extensions) {
-    $items = Get-ChildItem -Path .\ -Filter $ext -Recurse
+    $items = Get-ChildItem -Path .\ -Filter $ext -Recurse | Where-Object { -not (Should-Ignore $_.FullName) }
     # Loop through each item
     foreach ($item in $items) {
         # Check if the item is a file
