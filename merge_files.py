70200087f40168aeef2d9164fced36dd6e1194ee
Syed Aman <syednashikaman@gmail.com>
2024-03-14 07:12:25 -0400

    Ignore dirs

--- merge_files.py
+++ merge_files.py
@@ -2,22 +2,26 @@ import argparse
 import os
 from pathlib import Path
 import pyperclip
+import fnmatch
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Merge file contents.')
 parser.add_argument('--extensions', nargs='*', help='List of file extensions to include (e.g., txt, py). Default is all files.', default=None)
 parser.add_argument('--ignore', nargs='*', help='List of file extensions to ignore (e.g., json, log).', default=[])
+parser.add_argument('--ignore_dirs', nargs='*', help='List of directory patterns to ignore (supports wildcards, e.g., .git, *__pycache__*).', default=[])
 args = parser.parse_args()
 
 def main():
-    # Convert extensions to a set for faster lookups
+    # Convert extensions to sets for faster lookups
     extensions = set(args.extensions) if args.extensions is not None else None
     ignore_extensions = set(args.ignore)
 
     all_files_contents = []
 
     # Walk through all directories and files
-    for root, dirs, files in os.walk("."):
+    for root, dirs, files in os.walk(".", topdown=True):
+        # Filter out ignored directories using wildcard patterns
+        dirs[:] = [d for d in dirs if not any(fnmatch.fnmatch(d, pattern) for pattern in args.ignore_dirs)]
         for file in files:
             filepath = Path(root) / file
             # Check if file should be ignored based on its extension
